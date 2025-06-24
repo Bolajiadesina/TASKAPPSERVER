90c2a31c94dc0347ececb879f56c33426da2dbe5
Bolaji Adesina <adesina.bolaji@yahoo.com>
2025-06-24 22:37:22 +0100

    completed integration of delete feature

--- src/main/java/com/moj/taskAppServer/services/TaskServiceImplementation.java
+++ src/main/java/com/moj/taskAppServer/services/TaskServiceImplementation.java
@@ -22,6 +22,7 @@ public class TaskServiceImplementation implements TaskService {
     @Override
     public ResponseEntity<ResponseData> getAllTasks() {
         // Logic to retrieve all tasks
+       
         ResponseData response = taskRepository.findAll();
         if (!"00".equals(response.getResponseCode())) {
             return ResponseEntity.internalServerError().body(response);
@@ -35,7 +36,12 @@ public class TaskServiceImplementation implements TaskService {
 
     @Override
     public ResponseEntity<ResponseData> createTask(Task task) {
-        ResponseData response = taskRepository.save(task);
+        ResponseData response=null;
+         int count=5;
+        for (int i=0; i<=count; i++) {
+              response = taskRepository.save(task);
+        }
+      
         if (!"00".equals(response.getResponseCode())) {
             return ResponseEntity.internalServerError().body(response);
         }
@@ -57,6 +63,7 @@ public class TaskServiceImplementation implements TaskService {
 
     @Override
     public ResponseEntity<ResponseData> getTaskById(Long id) {
+        
      
         ResponseData response = taskRepository.findById(id);
         if (!"00".equals(response.getResponseCode())) {
